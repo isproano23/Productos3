966419f33d82e1331dbf43fdc5536b05b6db8cd2
Aantony <alexanderpillalaza1997@gmail.com>
2026-03-25 22:38:50 -0500

    atributos

--- Carrito.java
+++ Carrito.java
@@ -6,48 +6,5 @@ public class Carrito {
     private int cantidad;
 
   
-    public Carrito(String nombreCliente) {
-        this.nombreCliente = nombreCliente;
-        this.productos = new String[10];
-        this.precios = new double[10];
-        this.cantidad = 0;
-    }
-
    
-    public void agregarProducto(String nombre, double precio) {
-        if (cantidad < 10) {
-            productos[cantidad] = nombre;
-            precios[cantidad] = precio;
-            cantidad++;
-
-            System.out.println("Producto agregado: " + nombre + " ($" + precio + ")");
-        } else {
-            System.out.println("El carrito está lleno.");
-        }
-    }
-
-   
-    public void calcularTotal() {
-        double total = 0;
-
-        for (int i = 0; i < cantidad; i++) {
-            total += precios[i];
-        }
-
-        System.out.println("Total del carrito: $" + total);
-    }
-
-    public void mostrarResumen() {
-        double total = 0;
-
-        System.out.println("=== Carrito de " + nombreCliente + " ===");
-
-        for (int i = 0; i < cantidad; i++) {
-            System.out.println((i + 1) + ". " + productos[i] + " - $" + precios[i]);
-            total += precios[i];
-        }
-
-        System.out.println("Total: $" + total);
-    }
-    
 }
